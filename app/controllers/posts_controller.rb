36f7a322d8899f1a8942e7b5459c7f5ea702c43e
David Parry <david.parry@suranyami.com>
2016-01-29 14:46:09 +1100

    initial views, controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,39 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html
+      format.json {render json: @posts}
+    end
   end
-
+  
   # GET /posts/1
   # GET /posts/1.json
   def show
+    respond_to do |format|
+      format.html
+      format.json {render json: @post}
+    end
   end
-
+  
   # GET /posts/new
   def new
     @post = Post.new
   end
-
+  
   # GET /posts/1/edit
   def edit
   end
-
+  
   # POST /posts
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
+    
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
@@ -36,7 +44,7 @@ class PostsController < ApplicationController
       end
     end
   end
-
+  
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
@@ -50,7 +58,7 @@ class PostsController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
@@ -60,15 +68,15 @@ class PostsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:subject, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:subject, :body)
+  end
 end
